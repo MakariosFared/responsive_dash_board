204e219f1ce03bd4261f6bc278cf1a1457748e0a
Makarios <makarios.fared@gmail.com>
2024-04-24 21:22:39 +0200

    active and inactive item

--- lib/widgets/drawer_item_list_view.dart
+++ lib/widgets/drawer_item_list_view.dart
@@ -3,21 +3,30 @@ import 'package:responsive_dash_board/models/drawer_item_model.dart';
 import 'package:responsive_dash_board/utils/app_images.dart';
 import 'package:responsive_dash_board/widgets/drawer_item.dart';
 
-class DrawerItemListView extends StatelessWidget {
+class DrawerItemListView extends StatefulWidget {
   const DrawerItemListView({
     super.key,
   });
 
-  static const List<DrawerItemModel> item = [
-    DrawerItemModel(title: "Dashboard", image: Assets.imagesDashboard),
-    DrawerItemModel(title: "My Transaction", image: Assets.imagesMyTransctions),
-    DrawerItemModel(title: "Statistics", image: Assets.imagesStatistics),
-    DrawerItemModel(title: "Wallet Account", image: Assets.imagesWalletAccount),
-    DrawerItemModel(title: "My Investments", image: Assets.imagesMyInvestments),
+  @override
+  State<DrawerItemListView> createState() => _DrawerItemListViewState();
+}
+
+class _DrawerItemListViewState extends State<DrawerItemListView> {
+  int activeIndex = 0;
+
+  final List<DrawerItemModel> item = [
+    const DrawerItemModel(title: "Dashboard", image: Assets.imagesDashboard),
+    const DrawerItemModel(
+        title: "My Transaction", image: Assets.imagesMyTransctions),
+    const DrawerItemModel(title: "Statistics", image: Assets.imagesStatistics),
+    const DrawerItemModel(
+        title: "Wallet Account", image: Assets.imagesWalletAccount),
+    const DrawerItemModel(
+        title: "My Investments", image: Assets.imagesMyInvestments),
     // DrawerItemModel(title: "Setting system", image: Assets.imagesSettings),
     // DrawerItemModel(title: 'Logout account', image: Assets.imagesLogout),
   ];
-
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
@@ -25,12 +34,22 @@ class DrawerItemListView extends StatelessWidget {
       physics: const NeverScrollableScrollPhysics(),
       itemCount: item.length,
       itemBuilder: (context, index) {
-        return Padding(
-          padding: const EdgeInsets.only(
-            top: 20,
-          ),
-          child: DrawerItem(
-            drawerItemModel: item[index],
+        return GestureDetector(
+          onTap: () {
+            if (activeIndex != index) {
+              setState(() {
+                activeIndex = index;
+              });
+            }
+          },
+          child: Padding(
+            padding: const EdgeInsets.only(
+              top: 20,
+            ),
+            child: DrawerItem(
+              drawerItemModel: item[index],
+              isActive: activeIndex == index,
+            ),
           ),
         );
       },
