3850d675afdcbd423099fc7141613da54ec332f5
Makarios <makarios.fared@gmail.com>
2024-04-30 10:58:46 +0300

    custom background container widget refactor

--- lib/widgets/all_expensess.dart
+++ lib/widgets/all_expensess.dart
@@ -1,21 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_dash_board/widgets/all_expensess_item_list_view.dart';
 import 'package:responsive_dash_board/widgets/all_expesess_header.dart';
+import 'package:responsive_dash_board/widgets/custom_background_container.dart';
 
 class AllExpenses extends StatelessWidget {
   const AllExpenses({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(20),
-      decoration: ShapeDecoration(
-        color: Colors.white,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(12),
-        ),
-      ),
-      child: const Column(
+    return const CustomBackgroundContainer(
+      child: Column(
         children: [
           AllExpensesHeader(),
           SizedBox(height: 16),
@@ -25,3 +19,4 @@ class AllExpenses extends StatelessWidget {
     );
   }
 }
+
