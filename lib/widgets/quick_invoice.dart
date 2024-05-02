74cb8852c90a2fa5c4ae5d40ce8ce0fd82783716
Makarios <makarios.fared@gmail.com>
2024-05-02 20:07:03 +0300

    quick invoice form

--- lib/widgets/quick_invoice.dart
+++ lib/widgets/quick_invoice.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_dash_board/widgets/custom_background_container.dart';
 import 'package:responsive_dash_board/widgets/latest_transaction.dart';
+import 'package:responsive_dash_board/widgets/quick_invoice_form.dart';
 import 'package:responsive_dash_board/widgets/quick_invoice_header.dart';
 import 'package:responsive_dash_board/widgets/title_text_field.dart';
 
@@ -20,11 +21,9 @@ class QuickInvoice extends StatelessWidget {
           LatestTransaction(),
           Divider(
             height: 48,
+            color: Color(0xffF1F1F1),
           ),
-          TitleTextField(
-            title: 'Customer name',
-            hint: 'Type customer name',
-          )
+          QuickInvoiceForm(),
         ],
       ),
     );
