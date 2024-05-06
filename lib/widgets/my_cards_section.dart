5f28e4ac15e60efc5e93420badeebd2b930842ed
Makarios <makarios.fared@gmail.com>
2024-05-06 22:11:58 +0300

    finish the logic of dots indicator

--- lib/widgets/my_cards_section.dart
+++ lib/widgets/my_cards_section.dart
@@ -1,10 +1,29 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:responsive_dash_board/utils/app_styles.dart';
 import 'package:responsive_dash_board/widgets/dots_indicator.dart';
 import 'package:responsive_dash_board/widgets/my_cards_page_view.dart';
 
-class MyCardsSection extends StatelessWidget {
+class MyCardsSection extends StatefulWidget {
   const MyCardsSection({super.key});
+
+  @override
+  State<MyCardsSection> createState() => _MyCardsSectionState();
+}
+
+class _MyCardsSectionState extends State<MyCardsSection> {
+  late PageController pageController;
+  int currentPageIndex = 0;
+
+  @override
+  void initState() {
+    pageController = PageController();
+    pageController.addListener(() {
+      currentPageIndex = pageController.page!.();
+      setState(() {});
+    });
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,11 +39,15 @@ class MyCardsSection extends StatelessWidget {
         const SizedBox(
           height: 20,
         ),
-        const MyCardsPageView(),
+        MyCardsPageView(
+          pageController: pageController,
+        ),
         const SizedBox(
           height: 20,
         ),
-        const DotsIndicator(),
+        DotsIndicator(
+          currentPageIndex: currentPageIndex,
+        ),
       ],
     );
   }
