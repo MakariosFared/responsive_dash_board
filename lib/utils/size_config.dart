02c78a70f5fd61a5976a9a6255219781735bc30d
Makarios <makarios.fared@gmail.com>
2024-05-30 11:35:48 +0300

    detailed income chart finish

--- lib/utils/size_config.dart
+++ lib/utils/size_config.dart
@@ -1,11 +1,13 @@
+import 'package:flutter/material.dart';
+
 class SizeConfig {
   static const double desktop = 1200;
   static const double tablet = 800;
 
-  // static late double height, width;
+  static late double height, width;
 
-  // static init(context) {
-  //   height = MediaQuery.of(context).size.height;
-  //   width = MediaQuery.of(context).size.width;
-  // }
+  static init(context) {
+    height = MediaQuery.of(context).size.height;
+    width = MediaQuery.of(context).size.width;
+  }
 }
